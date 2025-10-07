3c4b85c74fe439f69788f5dd10779b9500eba678
inclined4x <jezemiahsam48@gmail.com>
2025-10-08 00:59:07 +0300

    Skeleton for the dashboard and getting the filters for 7, 30, 90 days of stays and bookings

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,10 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
+import Spinner from "../../ui/Spinner";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +12,20 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { bookings, isLoading: isLoading1 } = useRecentBookings();
+  const { stays, confirmedStays, isLoading: isLoading2 } = useRecentStays();
+
+  if (isLoading1 || isLoading2) return <Spinner />;
+
+  console.log(bookings);
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Todays act</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,21 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import DashboardLayout from "../features/dashboard/DashboardLayout";
+import DashboardFilter from "../features/dashboard/DashboardFilter";
 
 function Dashboard() {
   return (
-    <Row type="horizontal">
-      <Heading as="h1">Dashboard</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">Dashboard</Heading>
+        <DashboardFilter />
+      </Row>
+
+      <DashboardLayout />
+    </>
   );
 }
 
