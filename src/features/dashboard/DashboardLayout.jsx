248d0eacce114cc5fb42085c39a4fd449d33a744
inclined4x <jezemiahsam48@gmail.com>
2025-10-09 16:06:48 +0300

    displayed the pie chart

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -8,6 +8,7 @@ import Spinner from "../../ui/Spinner";
 import Stats from "./stats";
 import { useCabins } from "../cabins/useCabins";
 import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -38,6 +39,7 @@ function DashboardLayout() {
         cabinCount={cabins.length}
       />
       <div>Todays act</div>
+      <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
--- src/features/dashboard/SalesChart.jsx
+++ src/features/dashboard/SalesChart.jsx
@@ -66,8 +66,6 @@ function SalesChart({ bookings, numDays }) {
     end: new Date(),
   });
 
-  console.log(allDates);
-
   const data = allDates.map((date) => {
     return {
       label: format(date, "MM dd"),
@@ -79,8 +77,6 @@ function SalesChart({ bookings, numDays }) {
         .reduce((acc, cur) => acc + cur.extrasPrice, 0),
     };
   });
-
-  console.log(data);
 
   const colors = isDarkMode
     ? {
@@ -98,7 +94,10 @@ function SalesChart({ bookings, numDays }) {
 
   return (
     <StyledSalesChart>
-      <Heading as="h2">Sales</Heading>
+      <Heading as="h2">
+        Sales from {format(allDates.at(0), "MMM dd yyyy")} &mdash;{" "}
+        {format(allDates.at(-1), "MMM dd yyyy")}
+      </Heading>
 
       <ResponsiveContainer height={300} width="100%">
         <AreaChart data={data}>
